6b4166d163bf8b908513700aa22b1cd31ae6b4d0
bwlee0425 <bwlee0425@gmail.com>
2024-10-30 11:00:25 +0900

    관심 기업 선택

--- main.py
+++ main.py
@@ -3,9 +3,6 @@
 import streamlit as st
 from pages import page2
 
-def page2():
-    st.title("Second page")
-
 pg = st.navigation([
     st.Page(page2, title="Stock 데이터분석", icon=":material/favorite:"),
     st.Page("page1.py", title="캘리포니아 집값 예측", icon="😂"),
